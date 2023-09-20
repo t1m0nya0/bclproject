6944241aa2f81bb8c9e99675bde777ca3ca006ae
tamirlan-nurbulatuly <tamirlan@1fit.app>
2023-09-20 15:26:04 +0600

    fix

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,4 @@
-launch.sh
-export DJANGO_SETTINGS_MODULE=bilim_ai.settings
+export DJANGO_SETTINGS_MODULE=bclproject.settings
 python manage.py collectstatic --noinput
 echo 'Applying migrations...'
 python manage.py migrate
